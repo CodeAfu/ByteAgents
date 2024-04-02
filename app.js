201960e5c3f813520f5dc3d2238a159d09ffa1a8
CodeAfu <ibrahim.afzal1999@gmail.com>
2024-04-02 22:09:38 +0800

    Added url routes test template, refer for future use

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const app = express();
+
+const testRoute = require('./api/routes/test');
+
+app.use('/test', testRoute);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,13 @@
+
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    // console.log(`Server is listening on port number: ${port}`);
+    const url = `http://127.0.0.1:${port}`
+    console.log(`Link: ${url}`);
+});
