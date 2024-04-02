ac0e0eeed0991ebac529dc949355b1e72270f377
CodeAfu <ibrahim.afzal1999@gmail.com>
2024-04-02 22:20:37 +0800

    Installed morgan logger package: npm install --save morgan

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const testRoute = require('./api/routes/test');
 
+app.use(morgan('dev'));
+
+// Routes
 app.use('/test', testRoute);
+
+// Catch error if not part of routes above
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });  
+});
 
 module.exports = app;
