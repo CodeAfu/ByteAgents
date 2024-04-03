dc2bf8495574efc28db0582118dc032c74f9f1d5
CodeAfu <ibrahim.afzal1999@gmail.com>
2024-04-03 17:17:03 +0800

    Use route template -> test.js

--- backend/api/routes/test.js
+++ backend/api/routes/test.js
@@ -1,21 +1,40 @@
-
-const express = require('express');
+const express = require('express')
 const router = express.Router();
 
+// GET all
 router.get('/', (req, res) => {
   res.status(200).json({
-    request: "GET"
+    request: req.method
   });
 });
 
-router.post('/', (req, res, next) => {
-  const object = {
-    name: req.body.name,
-      price: req.body.price
-  };
-  res.status(201).json({
-    request: "POST:",
-    createdObject: object
+// GET one
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  res.status(200).json({
+    request: req.method,
+    id : id
+  });
+});
+
+// UPDATE
+router.patch('/', (req, res) => {
+  res.status(200).json({
+    request: req.method
+  });
+});
+
+// DELETE
+router.delete('/', (req, res) => {
+  res.status(200).json({
+    request: req.method
+  });
+});
+
+// POST
+router.post('/', (req, res) => {
+  res.status(200).json({
+    request: req.method
   });
 });
 
